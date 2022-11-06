ecfdb9128b05e1f69e2b2c3af77d56bd2120e16a
Dmytro Shkarupa <dmytro.shkarupa@Dmytro.Shkarupa>
2022-11-06 17:21:00 +0200

    [lw3] Update Model API; [cw1] Publish my solution

---
+++ cw1/bank.py
@@ -0,0 +1,68 @@
+import random
+from functools import partial
+import sys
+from pathlib import Path
+from typing import Any
+
+sys.path.append(str(Path(__file__).parents[1].joinpath('lw3')))
+
+# pylint: disable=wrong-import-position, import-error
+from lib.base import Item
+from lib.factory import FactoryNode
+from lib.queueing import QueueingNode, Queue
+from lib.model import Model
+
+
+class BankFactoryNode(FactoryNode):
+
+    def __init__(self, max_num_in_bank: int, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.max_num_in_bank = max_num_in_bank
+        self.operator: QueueingNode[Item] = None
+        self.cachier: QueueingNode[Item] = None
+
+    @property
+    def num_in_bank(self) -> float:
+        return self.operator.queuelen + self.cachier.queuelen + self.cachier.num_channels + self.operator.num_channels
+
+    def set_bank_nodes(self, operator: QueueingNode[Item], cachier: QueueingNode[Item]) -> None:
+        self.operator = operator
+        self.cachier = cachier
+
+    # pylint: disable=attribute-defined-outside-init
+    def end_action(self) -> Item:
+        self.item = self._get_next_item()
+        self.metrics.items.append(self.item)
+        self.next_time = self._predict_next_time()
+        failure = self.num_in_bank > self.max_num_in_bank
+        if failure:
+            self.next_node, next_node = None, self.next_node
+        out_item = self._end_action(self.item)
+        if failure:
+            self.next_node = next_node
+        return out_item
+
+
+def run_simulation() -> None:
+    factory = BankFactoryNode(name='1. Factory',
+                              max_num_in_bank=10,
+                              delay_fn=partial(random.expovariate, lambd=1.0 / 3.0))
+    operator = QueueingNode[Item](name='2. Operator',
+                                  queue=Queue(),
+                                  max_channels=1,
+                                  delay_fn=partial(random.uniform, a=1, b=5))
+    cachier = QueueingNode[Item](name='3. Cachier',
+                                 queue=Queue(),
+                                 max_channels=1,
+                                 delay_fn=partial(random.uniform, a=2, b=4))
+
+    factory.set_bank_nodes(operator=operator, cachier=cachier)
+    factory.set_next_node(operator)
+    operator.set_next_node(cachier)
+
+    model = Model.from_factory(factory)
+    model.simulate(end_time=1000)  # verbosity=Verbosity.METRICS | Verbosity.STATE
+
+
+if __name__ == '__main__':
+    run_simulation()
--- lw3/lib/model.py
+++ lw3/lib/model.py
@@ -1,12 +1,14 @@
 from enum import Flag, auto
 from dataclasses import dataclass, field
-from typing import Callable, Generic, Optional, Type, Any
+from typing import Callable, Generic, Optional, Type, TypeVar, Any
 
 from lib.common import INF_TIME, TIME_EPS, T
 from lib.base import Node, Metrics, NodeMetrics
 from lib.factory import BaseFactoryNode
 from lib.queueing import QueueingNode
 from lib.logger import BaseLogger, Logger
+
+M = TypeVar('M', bound='Model')
 
 
 @dataclass(eq=False)
@@ -31,13 +33,12 @@ class Evaluation(Generic[T]):
 
 
 @dataclass(eq=False)
-class ModelMetrics(Metrics[T]):
+class ModelMetrics(Metrics[M]):
     num_events: int = field(init=False, default=0)
-    simulation_time: float = field(init=False, default=0)
 
     @property
     def mean_event_intensity(self) -> float:
-        return self.num_events / max(self.simulation_time, TIME_EPS)
+        return self.num_events / max(self.parent.current_time, TIME_EPS)
 
 
 class Verbosity(Flag):
@@ -62,49 +63,58 @@ class Model(Generic[T]):
         self.logger = logger
         self.metrics = metrics_type[Model[T]](self)
         self.evaluations = [] if evaluations is None else evaluations
+        self.current_time = 0
 
     def simulate(self, end_time: float, verbosity: Verbosity = Verbosity.METRICS) -> ModellingMetrics:
-        current_time = 0
-        while current_time < end_time:
-            # Find next closest action
-            next_time = INF_TIME
-            for node in self.nodes:
-                if node.next_time < next_time:
-                    next_time = node.next_time
-            if next_time > end_time:
+        while self.current_time < end_time:
+            # Run simulation step
+            updated_nodes = self.step(end_time)
+            if updated_nodes is None:
                 break
-            # Move to that action
-            current_time = next_time
-            for node in self.nodes:
-                node.update_time(current_time)
-            # Select nodes to be updated now
-            updated_nodes: list[Node[T]] = []
-            for node in self.nodes:
-                if abs(next_time - node.next_time) > TIME_EPS:
-                    continue
-                updated_nodes.append(node)
-            # Run actions
-            for node in updated_nodes:
-                node.end_action()
-                if isinstance(node, (BaseFactoryNode, QueueingNode)):
-                    self.metrics.num_events += 1
-            self.metrics.simulation_time = current_time
             # Log state
             if Verbosity.STATE in verbosity:
-                self.logger.log_state(current_time, self.nodes, updated_nodes)
+                self.logger.log_state(self.current_time, self.nodes, updated_nodes)
         # Collect metrics
-        nodes_metrics = [node.metrics for node in self.nodes]
-        evaluation_reports = [evaluation(self) for evaluation in self.evaluations]
-        modelling_metrics = self.metrics, nodes_metrics, evaluation_reports
+        metrics = self.get_metrics()
         # Log metrics
         if Verbosity.METRICS in verbosity:
-            self.logger.log_metrics(*modelling_metrics)
-        return modelling_metrics
+            self.logger.log_metrics(*metrics)
+        return metrics
+
+    def step(self, end_time: Optional[float] = None) -> Optional[list[Node[T]]]:
+        if end_time is not None and self.current_time > end_time:
+            return None
+        # Find next closest action
+        next_time = min(INF_TIME, *(node.next_time for node in self.nodes))
+        if end_time is not None and next_time > end_time:
+            return None
+        # Move to that action
+        self.current_time = next_time
+        for node in self.nodes:
+            node.update_time(self.current_time)
+        # Select nodes to be updated now
+        updated_nodes: list[Node[T]] = []
+        for node in self.nodes:
+            if abs(next_time - node.next_time) > TIME_EPS:
+                continue
+            updated_nodes.append(node)
+        # Run actions
+        for node in updated_nodes:
+            node.end_action()
+            if isinstance(node, (BaseFactoryNode, QueueingNode)):
+                self.metrics.num_events += 1
+        return updated_nodes
 
     def reset(self) -> None:
+        self.current_time = 0
         for node in self.nodes:
             node.reset()
         self.metrics.reset()
+
+    def get_metrics(self) -> ModellingMetrics:
+        nodes_metrics = [node.metrics for node in self.nodes]
+        evaluation_reports = [evaluation(self) for evaluation in self.evaluations]
+        return self.metrics, nodes_metrics, evaluation_reports
 
     @staticmethod
     def from_factory(factory: Node[T], **kwargs: Any) -> 'Model[T]':
